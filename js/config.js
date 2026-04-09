772b4b2a89a639ef23a937e207869001410c72b9
Clara Cao <claracao@MacBookPro.lan>
2026-04-09 03:37:35 +0300

    Use URL-restricted public Mapbox token

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 var APP_CONFIG = {
-  mapboxToken: 'pk.eyJ1IjoiY2xhcmF4Y2FvIiwiYSI6ImNtbnB3bnp5ZDAxaG0ycnNkcWVzYzJxMG0ifQ.8BSXIH1PTEY4of7kd_1aEg'
+  mapboxToken: 'pk.eyJ1IjoiY2xhcmF4Y2FvIiwiYSI6ImNtbnFxeXFrdTAwYjgyb3NuYTk2bXRpYmwifQ.g8C4ysMoDInz3aDdoLS1Nw'
 };
